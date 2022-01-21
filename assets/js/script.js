018de3aff56e68d7891b6a0849f3115e1a558ca3
Jon Mooney <jon.mooney@yahoo.com>
2022-01-21 15:09:54 -0800

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,7 +58,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -183,18 +183,21 @@ $(".card .list-group").sortable({
   cursor: "move",
   opacity: 0.7,
   helper: "clone",
-  activate: function(event, ui) {
-    ui.helper.addClass("highlight");
+  activate: function(event) {
+    // ui.helper.addClass("highlight");
+    //ui.helper.addClass("dropover");
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
-  deactivate: function(event, ui) {
-    //ui.helper.removeClass("highlight");
+  deactivate: function(event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    //console.log("over", event.target);
-    
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    //console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
     // array to store the task data in
@@ -242,9 +245,11 @@ $("#trash").droppable({
   },
   over: function(event, ui) {
     console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
     console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -284,6 +289,12 @@ var auditTask = function(taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 };
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 
 // load tasks for the first time
 loadTasks();
